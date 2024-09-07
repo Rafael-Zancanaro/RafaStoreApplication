dde0cf998d4f93b37eb247b2ed41d2e79abe1e4c
Rafael Zancanaro Cardoso <rafa10_zc@hotmail.com>
2024-09-07 11:02:34 -0300

    tratar erros

---
+++ src/web/RafaStore.WebApp.MVC/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RafaStore.WebApp.MVC.Models;
+
+namespace RafaStore.WebApp.MVC.Controllers
+{
+    public class MainController : Controller
+    {
+        protected bool ResponsePossuiErros(ResponseResult response)
+        {
+            if (response != null && response.Errors.Mensagens.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/web/RafaStore.WebApp.MVC/Extensions/IUser.cs
@@ -0,0 +1,85 @@
+﻿using System.Security.Claims;
+
+namespace RafaStore.WebApp.MVC.Extensions
+{
+    public interface IUser
+    {
+        string Name { get; }
+        Guid ObterUserId();
+        string ObterUserEmail();
+        string ObterUserToken();
+        bool EstaAutenticado();
+        bool PossuiRole(string role);
+        IEnumerable<Claim> ObterClaims();
+        HttpContext ObterHttpContext();
+    }
+
+    public class AspNetUser(IHttpContextAccessor contextAccessor) : IUser
+    {
+        private readonly IHttpContextAccessor _contextAccessor = contextAccessor;
+
+        public string Name => _contextAccessor.HttpContext.User.Identity.Name;
+
+        public bool EstaAutenticado()
+        {
+            return _contextAccessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public IEnumerable<Claim> ObterClaims()
+        {
+            return _contextAccessor.HttpContext.User.Claims;
+        }
+
+        public HttpContext ObterHttpContext()
+        {
+            return _contextAccessor.HttpContext;
+        }
+
+        public string ObterUserEmail()
+        {
+            return EstaAutenticado() ? _contextAccessor.HttpContext.User.GetUserEmail() : string.Empty;
+        }
+
+        public Guid ObterUserId()
+        {
+            return EstaAutenticado() ? Guid.Parse(_contextAccessor.HttpContext.User.GetUserId()) : Guid.Empty;
+        }
+
+        public string ObterUserToken()
+        {
+            return EstaAutenticado() ? _contextAccessor.HttpContext.User.GetUserToken() : string.Empty;
+        }
+
+        public bool PossuiRole(string role)
+        {
+            return _contextAccessor.HttpContext.User.IsInRole(role);
+        }
+    } 
+
+    public static class ClaimPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            ArgumentNullException.ThrowIfNull(principal);
+
+            var claim = principal.FindFirst("sub");
+            return claim?.Value;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            ArgumentNullException.ThrowIfNull(principal);
+
+            var claim = principal.FindFirst("email");
+            return claim?.Value;
+        }
+
+        public static string GetUserToken(this ClaimsPrincipal principal)
+        {
+            ArgumentNullException.ThrowIfNull(principal);
+
+            var claim = principal.FindFirst("JWT");
+            return claim?.Value;
+        }
+    }
+}
