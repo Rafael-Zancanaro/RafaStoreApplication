367834c4acec619cee4c1f2ea2c766d8ad085f18
Rafael Zancanaro Cardoso <rafa10_zc@hotmail.com>
2024-09-11 23:09:30 -0300

    finalizando api de mvc

---
+++ src/services/RafaStore.Catalogo.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RafaStore.Catalogo.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- src/web/RafaStore.WebApp.MVC/Program.cs
+++ src/web/RafaStore.WebApp.MVC/Program.cs
@@ -1,12 +1,24 @@
 using RafaStore.WebApp.MVC.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
 var services = builder.Services;
 
 //Services
 services.AddIdentityConfiguration();
 
-services.AddMvcConfiguration();
+services.AddMvcConfiguration(builder.Configuration);
 
 services.RegisterServices();
 
