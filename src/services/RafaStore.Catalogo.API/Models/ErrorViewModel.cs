43e8793a83a1f7e607b626a1744bbe266efbc005
Rafael Zancanaro Cardoso <rafa10_zc@hotmail.com>
2024-09-22 11:12:03 -0300

    limpeza da camada catalogo

--- src/services/RafaStore.Catalogo.API/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RafaStore.Catalogo.API.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
