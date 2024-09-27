d8fbdb58b8aea286cf5adfb896cd6a964472feae
Rafael Zancanaro Cardoso <rafa10_zc@hotmail.com>
2024-09-27 07:49:32 -0300

    configuracao api de catagolo

---
+++ src/services/RafaStore.Catalogo.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,14 @@
+﻿using RafaStore.Catalogo.API.Data.Repository;
+using RafaStore.Catalogo.API.Data;
+using RafaStore.Catalogo.API.Models;
+
+namespace RafaStore.Catalogo.API.Configuration;
+
+public static class DependencyInjectionConfig
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProdutoRepository, ProdutoRepository>();
+        services.AddScoped<CatalogoContext>();
+    }
+}
---
+++ src/services/RafaStore.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace RafaStore.Catalogo.API.Configuration;
+
+public static class SwaggerConfig
+{
+    public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "RafaStore enterprise Identity API",
+                Description = "Esta API é responsável por toda parte de autenticação",
+                Contact = new OpenApiContact() { Name = "Rafael Zancanaro Cardoso", Email = "rafa10_zc@hotmail.com" },
+                License = new OpenApiLicense()
+                {
+                    Name = "MIT",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app, IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
+
+        return app;
+    }
+}
